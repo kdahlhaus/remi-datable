b470bdaa9ee1e31c529678d13624db85ac96ff20
Kevin Dahlhausen <kevin.dahlhausen@gmail.com>
2017-08-19 00:11:32 -0400

    rename

--- datatable.py
+++ datatable.py
@@ -1,14 +1,20 @@
-class DataTableWithData(gui.Widget):
+import remi.gui as gui
+
+import logging
+log = logging.getLogger("datatable")
+
+
+class DataTableWithDomData(gui.Widget):
 
     @gui.decorate_constructor_parameter_types([dict,  ])
     def __init__(self, data_table_options={}, **kwargs):
         "data_table_options = dictionay of data table options.  see  https://datatables.net/reference/option/"
         self.rows=[]
         self.data_table_options=data_table_options
-        logging.debug("DataTable.__iniit__ kwargs=%s"%kwargs)
-        super(DataTableWithData, self).__init__(**kwargs)
-        logging.debug("DT style after init %s"%(self.style))
-        logging.debug("DT attributes after init %s"%(self.attributes))
+        log.debug("DataTable.__iniit__ kwargs=%s"%kwargs)
+        super(DataTableWithDomData, self).__init__(**kwargs)
+        log.debug("DT style after init %s"%(self.style))
+        log.debug("DT attributes after init %s"%(self.attributes))
 
     def set_column_headings(self, headings):
         self.column_headings = headings
@@ -17,8 +23,8 @@ class DataTableWithData(gui.Widget):
         self.rows.append(row)
 
     def repr(self, client, changed_widgets={}):
-        #logging.debug("datatable repr self.style:%s"%str(self.style))
-        #logging.debug("datatable repr self.attributes:%s"%str(self.attributes))
+        #log.debug("datatable repr self.style:%s"%str(self.style))
+        #log.debug("datatable repr self.attributes:%s"%str(self.attributes))
         self.attributes['style'] = gui.jsonize(self.style)
         attribute_string = ' '.join('%s="%s"' % (k, v) if v is not None else k for k, v in
                                                 self.attributes.items())
